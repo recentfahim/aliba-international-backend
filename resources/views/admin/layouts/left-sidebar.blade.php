0b48c65b1903fbfbad7d8e962cb926deb8b30034
Fahim Ahmed <recentfahim13@gmail.com>
2020-11-05 23:53:51 +0600

    seperate layout and dashboard

---
+++ resources/views/admin/layouts/left-sidebar.blade.php
@@ -0,0 +1,63 @@
+<div class="scrollbar-sidebar">
+    <div class="app-sidebar__inner">
+        <ul class="vertical-nav-menu">
+            <li class="app-sidebar__heading">Settings</li>
+            <li>
+                <a href="charts-chartjs.html">
+                    <i class="metismenu-icon pe-7s-graph2">
+                    </i>Currency Conversion
+                </a>
+            </li>
+            <li>
+                <a href="charts-chartjs.html">
+                    <i class="metismenu-icon pe-7s-graph2">
+                    </i>Location
+                </a>
+            </li>
+            <li class="app-sidebar__heading">Orders</li>
+            <li>
+                <a href="dashboard-boxes.html">
+                    <i class="metismenu-icon pe-7s-display2"></i>
+                    Orders
+                </a>
+            </li>
+            <li>
+                <a href="dashboard-boxes.html">
+                    <i class="metismenu-icon pe-7s-display2"></i>
+                    Order Payments
+                </a>
+            </li>
+            <li>
+                <a href="dashboard-boxes.html">
+                    <i class="metismenu-icon pe-7s-display2"></i>
+                    Order Products
+                </a>
+            </li>
+            <li>
+                <a href="dashboard-boxes.html">
+                    <i class="metismenu-icon pe-7s-display2"></i>
+                    Payment History
+                </a>
+            </li>
+            <li>
+                <a href="dashboard-boxes.html">
+                    <i class="metismenu-icon pe-7s-display2"></i>
+                    Order Status
+                </a>
+            </li>
+            <li class="app-sidebar__heading">Users</li>
+            <li>
+                <a href="forms-controls.html">
+                    <i class="metismenu-icon pe-7s-mouse">
+                    </i>Users
+                </a>
+            </li>
+            <li>
+                <a href="forms-layouts.html">
+                    <i class="metismenu-icon pe-7s-eyedropper">
+                    </i>Address
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
