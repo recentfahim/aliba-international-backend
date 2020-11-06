d7521a9d5cc9e7ee591709510e526f94abc1008e
Fahim Ahmed <recentfahim13@gmail.com>
2020-11-06 12:52:02 +0600

    added new column in order, show demo data in order table

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
 use Illuminate\Http\Request;
 
 class AdminOrderController extends Controller
 {
     public function index(){
-        return view('admin.order.index');
+        $orders = Order::all();
+
+        return view('admin.order.index', compact('orders'));
     }
 }
