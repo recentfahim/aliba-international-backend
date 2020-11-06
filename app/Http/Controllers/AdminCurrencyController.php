ba40c14b7f596cf12f0c1727a4efd5f4736c4399
Fahim Ahmed <recentfahim13@gmail.com>
2020-11-06 12:24:06 +0600

    working on currency setup

--- app/Http/Controllers/AdminCurrencyController.php
+++ app/Http/Controllers/AdminCurrencyController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Currency;
 use Illuminate\Http\Request;
 
 class AdminCurrencyController extends Controller
 {
     public function index(){
-        return view('admin.currency.index');
+        $currency = Currency::all()->last();
+
+        return view('admin.currency.index', compact('currency'));
     }
 }
--- resources/views/admin/currency/index.blade.php
+++ resources/views/admin/currency/index.blade.php
@@ -8,122 +8,24 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="main-card mb-3 card">
-                    <div class="card-header">Latest Orders</div>
+                    <div class="card-header">Currency Conversion</div>
                     <div class="table-responsive">
                         <table class="align-middle mb-0 table table-borderless table-striped table-hover">
                             <thead>
                             <tr>
-                                <th class="text-center">ID</th>
-                                <th>Name</th>
-                                <th class="text-center">Phone</th>
-                                <th class="text-center">City</th>
-                                <th class="text-center">Status</th>
-                                <th class="text-center">Actions</th>
+                                <th>Received Currency</th>
+                                <th>BDT</th>
+                                <th>Actions</th>
                             </tr>
                             </thead>
                             <tbody>
                             <tr>
-                                <td class="text-center text-muted">#345</td>
+                                <td class="text-weight-bold">{{ $currency->received_currency }}</td>
+                                <td>{{ $currency->bdt }}</td>
                                 <td>
-                                    <div class="widget-content p-0">
-                                        <div class="widget-content-wrapper">
-                                            <div class="widget-content-left mr-3">
-                                                <div class="widget-content-left">
-                                                    <img width="40" class="rounded-circle" src="assets/images/avatars/4.jpg" alt="">
-                                                </div>
-                                            </div>
-                                            <div class="widget-content-left flex2">
-                                                <div class="widget-heading">John Doe</div>
-                                                <div class="widget-subheading opacity-7">Web Developer</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="text-center">012318748121</td>
-                                <td class="text-center">Madrid</td>
-                                <td class="text-center">
-                                    <div class="badge badge-warning">Pending</div>
-                                </td>
-                                <td class="text-center">
-                                    <button type="button" id="PopoverCustomT-1" class="btn btn-primary btn-sm">Details</button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-center text-muted">#347</td>
-                                <td>
-                                    <div class="widget-content p-0">
-                                        <div class="widget-content-wrapper">
-                                            <div class="widget-content-left mr-3">
-                                                <div class="widget-content-left">
-                                                    <img width="40" class="rounded-circle" src="assets/images/avatars/3.jpg" alt="">
-                                                </div>
-                                            </div>
-                                            <div class="widget-content-left flex2">
-                                                <div class="widget-heading">Ruben Tillman</div>
-                                                <div class="widget-subheading opacity-7">Etiam sit amet orci eget</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="text-center">012318748121</td>
-                                <td class="text-center">Berlin</td>
-                                <td class="text-center">
-                                    <div class="badge badge-success">Completed</div>
-                                </td>
-                                <td class="text-center">
-                                    <button type="button" id="PopoverCustomT-2" class="btn btn-primary btn-sm">Details</button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-center text-muted">#321</td>
-                                <td>
-                                    <div class="widget-content p-0">
-                                        <div class="widget-content-wrapper">
-                                            <div class="widget-content-left mr-3">
-                                                <div class="widget-content-left">
-                                                    <img width="40" class="rounded-circle" src="assets/images/avatars/2.jpg" alt="">
-                                                </div>
-                                            </div>
-                                            <div class="widget-content-left flex2">
-                                                <div class="widget-heading">Elliot Huber</div>
-                                                <div class="widget-subheading opacity-7">Lorem ipsum dolor sic</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="text-center">012318748121</td>
-                                <td class="text-center">London</td>
-                                <td class="text-center">
-                                    <div class="badge badge-danger">In Progress</div>
-                                </td>
-                                <td class="text-center">
-                                    <button type="button" id="PopoverCustomT-3" class="btn btn-primary btn-sm">Details</button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-center text-muted">#55</td>
-                                <td>
-                                    <div class="widget-content p-0">
-                                        <div class="widget-content-wrapper">
-                                            <div class="widget-content-left mr-3">
-                                                <div class="widget-content-left">
-                                                    <img width="40" class="rounded-circle" src="assets/images/avatars/1.jpg" alt="">
-                                                </div>
-                                            </div>
-                                            <div class="widget-content-left flex2">
-                                                <div class="widget-heading">Vinnie Wagstaff</div>
-                                                <div class="widget-subheading opacity-7">UI Designer</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="text-center">012318748121</td>
-                                <td class="text-center">Amsterdam</td>
-                                <td class="text-center">
-                                    <div class="badge badge-info">On Hold</div>
-                                </td>
-                                <td class="text-center">
-                                    <button type="button" id="PopoverCustomT-4" class="btn btn-primary btn-sm">Details</button>
+                                    <span class="text-info">
+                                        <i class="fas fa-edit"></i>
+                                    </span>
                                 </td>
                             </tr>
                             </tbody>
