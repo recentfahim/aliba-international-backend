01e9a88285ff5ad1853e0a49723ff1b6f36342f8
Fahim Ahmed <recentfahim13@gmail.com>
2020-11-20 20:46:16 +0600

    typo fixed in brand controller

--- app/Http/Controllers/Api/BrandController.php
+++ app/Http/Controllers/Api/BrandController.php
@@ -8,12 +8,12 @@ use Illuminate\Http\Request;
 class BrandController extends Controller
 {
     public function rootBrand(Request $request){
-        $categories = OTCRequest('GetBrandInfoList');
+        $brands = OTCRequest('GetBrandInfoList');
 
-        if($categories){
-            return response()->json(['data' => $categories->BrandInfoList->Content, 'status' => 'Found', 'success' => true], 200);
+        if($brands){
+            return response()->json(['data' => $brands->BrandInfoList->Content, 'status' => 'Found', 'success' => true], 200);
         } else {
-            return response()->json(['data' => array(), 'status' => 'No Category Found', 'success' => false], 200);
+            return response()->json(['data' => array(), 'status' => 'No Brand Found', 'success' => false], 200);
         }
     }
 }
